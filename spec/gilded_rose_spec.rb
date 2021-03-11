355731f41de8112d7a2c62a2202d8c8020e78c75
Hamish <hamisharro@icloud.com>
2021-03-11 23:06:31 +0000

    add tests for backstage

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -14,6 +14,24 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 1
     end
+
+    it " [Backstage] changes the quality by +1 when sell_in = 15" do
+      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 21
+    end
+
+    it " [Backstage] changes the quality by +2 when sell_in = 10" do
+      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 22
+    end
+
+    it " [Backstage] changes the quality by +3 when sell_in = 5" do
+      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 23
+    end
   end
 
 end
