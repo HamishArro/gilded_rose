cee202411f4bdc80dde0214071a32eaf5eae98c4
Hamish <hamisharro@icloud.com>
2021-03-11 23:15:42 +0000

    corrected rspec layout

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,46 +3,76 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it " [normal] changes the quality by -1" do
-      items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 19
+
+    describe '[normal]' do
+
+      it "changes the quality by -1" do
+        items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 19
+        expect(items[0].sell_in).to eq 9
+      end
+
     end
 
-    it " [Brie] changes the quality by +1" do
-      items = [Item.new(name="Aged Brie", sell_in=2, quality=0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 1
+    describe '[Brie]' do
+
+      it "changes the quality by +1" do
+        items = [Item.new(name="Aged Brie", sell_in=2, quality=0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 1
+        expect(items[0].sell_in).to eq 1
+      end
+
     end
 
-    it " [Backstage] changes the quality by +1 when sell_in = 15" do
-      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 21
+    describe '[Backstage]' do
+
+      it "changes the quality by +1 when sell_in = 15" do
+        items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 21
+        expect(items[0].sell_in).to eq 14
+      end
+
+      it "changes the quality by +2 when sell_in = 10" do
+        items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 22
+        expect(items[0].sell_in).to eq 9
+      end
+
+      it "changes the quality by +3 when sell_in = 5" do
+        items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 23
+        expect(items[0].sell_in).to eq 4
+      end
+
+      it "changes the quality to 0 when sell_in = 0" do
+        items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+        expect(items[0].sell_in).to eq -1
+      end
+
     end
 
-    it " [Backstage] changes the quality by +2 when sell_in = 10" do
-      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 22
-    end
+    describe '[Sulfuras]' do
 
-    it " [Backstage] changes the quality by +3 when sell_in = 5" do
-      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=20)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 23
-    end
+      it "unchanged when sell_in = 0" do
+        items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 80
+      end
 
-    it " [Sulfuras] unchanged when sell_in = 0" do
-      items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 80
-    end
+      it "unchanged when sell_in = -1" do
+        items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 80
+        expect(items[0].sell_in).to eq -1
+      end
 
-    it " [Sulfuras] unchanged when sell_in = -1" do
-      items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 80
     end
   end
 
